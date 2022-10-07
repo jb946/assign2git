85649ab4d32668ce3869675fb9e2a94cff4015f4
Jean-Baptiste Abrassart <jean-baptiste.abrassart@efrei.net>
2022-10-07 12:34:41 -0700

    JS&CSSfile

--- index.js
+++ index.js
@@ -2,9 +2,10 @@ const express = require('express')
 const app = express()
 const port = 3000
 
-app.get('/', (req, res) => {
-    res.send('Hello World!')
-})
+/*app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/index.html')
+})*/
+app.use(express.static('public'));
 
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
