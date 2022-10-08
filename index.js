3c3487edaeb7ea92228762bf53bd18dce2ccd4dd
Jean-Baptiste Abrassart <jean-baptiste.abrassart@efrei.net>
2022-10-07 17:10:27 -0700

    api&JSupdate

--- index.js
+++ index.js
@@ -2,9 +2,9 @@ const express = require('express')
 const app = express()
 const port = 3000
 
-/*app.get('/', (req, res) => {
-    res.sendFile(__dirname + '/index.html')
-})*/
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/public/index.html')
+})
 app.use(express.static('public'));
 
 app.listen(port, () => {
--- public/jsindex.js
+++ public/jsindex.js
@@ -1,1 +1,38 @@
-console.log("Hello world");
+console.log("Hello world\n");
+let date1 = Date.now();
+console.log(date1);
+const api_url = "https://www.thesportsdb.com/api/v1/json/2/searchevents.php?e=";
+
+function search_team(){
+    var select = document.getElementById('team');
+    var value = select.options[select.selectedIndex].value;
+    team_name = value;
+    get_event(team_name);
+}
+
+
+
+async function get_event(team_name){
+    const response = await fetch(api_url+team_name);
+    response.json().then(value =>{
+        let i;
+        let index;
+        for(i=24;i>=0;i--){
+            if(date1<=Date.parse(value.event[i].strTimestamp)){
+                index = i;
+                i=-1;
+            }
+        }
+
+        console.log(value.event[index].strFilename);
+
+
+        //integration in the HTML
+        document.getElementById("result").innerHTML = value.event[index].strFilename;
+    });
+    //const data = await response.json();
+
+
+    //const obj = JSON.parse(data);
+}
+
